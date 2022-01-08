65d483ed5bad9c034aff78835da1d2ecab6d2be9
amjadcp <amjadcp03@gmail.com>
2022-01-08 23:42:17 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import Todo from "./component/Todo";
 const App =()=> {
   return (
    <section className="vh-100" style={{backgroundColor: "#e2d5de"}}>
-       <h6 className="mb-3">Awesome Todo List</h6>
-       <Todo/>
+     <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col col-xl-10">
+           <div className="card" style={{borderradius:"15px"}}>
+              <div className="card-body p-5">
+                    <h6 className="mb-3">Awesome Todo List</h6>
+                    <Todo/>
+              </div>
+           </div>
+          </div>
+        </div>
+      </div>
    </section>
     
   );
--- src/component/Todo.js
+++ src/component/Todo.js
@@ -5,58 +5,59 @@ const Todo =()=>{
     return(
 
         <div>
-            <input type="text" name="todo" id="todo" />
-            &nbsp;&nbsp;
-            <button onClick={
-                ()=>{
-                    let task = document.getElementById("todo").value
-                    document.getElementById("todo").value = ''
-                    return setToDos([...toDos, {'task':task, 'status':false}])
-                    }}>
+            {/* <form className="d-flex justify-content-center align-items-center mb-4"> */}
+                <div className="form-outline flex-fill">
+                    <input type="text" name="todo" id="todo" className="form-control form-control-lg"/>
+                    <label className="form-label">What do you need to do today?</label>
+                </div>
+            {/* </form> */}
+            <button className="btn btn-primary btn-lg ms-2"
+                onClick={
+                    ()=>{
+                        let task = document.getElementById("todo").value
+                        document.getElementById("todo").value = ''
+                        return setToDos([...toDos, {'task':task, 'status':false}])
+                        }}
+            >
             Add
             </button>
-            <br />
-
             {
-                toDos.map((data, key)=>{
+                toDos.map( (data, key)=>{
                     return(
-                            <div id={key} key={key}>
-                                    {data.status ? <strike>{data.task}</strike> : data.task}
-                                    <input type="checkbox" name={data.task} id={data.task} value={data.task}
-
-                                        onChange={
-                                            (e)=>{
+                        <div id={key} key={key}>
+                          <ul className="list-group mb-0">
+                              <li className="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
+                                    <div className="d-flex align-items-center">
+                                        <input className="form-check-input me-2" type="checkbox" value="" aria-label="..." 
+                                            onChange={
+                                                        (e)=>{
+                                                            let i = toDos.indexOf(data)
+                                                            toDos[i].status = e.target.checked
+                                                            return setToDos([...toDos])
+                                                        }
+                                                    }
+                                        />
+                                        {data.status ? <strike>{data.task}</strike> : data.task}
+                                    </div>
+                                    <a href="#!" data-mdb-toggle="tooltip" title="Remove item">
+                                        <i className="fas fa-times text-primary"
+                                             onClick={ ()=>{
                                                 let i = toDos.indexOf(data)
-                                                toDos[i].status = e.target.checked
+                                                toDos.splice(i, 1)
                                                 return setToDos([...toDos])
-                                                            // toDos.filter(data_copy=>{
-                                                            //     if(data_copy.task === data.task){
-                                                            //         data_copy.status = e.target.checked
-                                                            //     }
-                                                            //     return data_copy
-                                                            // })
-                                                console.log(toDos);
-                                            }
-                                        }
-                                    />
-                                    <button
-                                       onClick={ ()=>{
-                                           let i = toDos.indexOf(data)
-                                           toDos.splice(i, 1)
-                                           return setToDos([...toDos])
-                                       }}
-                                    >
-                                        Delete
-                                    </button>
-                            </div>
-                    )
+                                            }}
+                                        ></i>
+                                    </a>
+                              </li>
+                          </ul>
+
+                        </div>
+                    );
                 })
             }
-        </div>
-
+        </div>                   
     );
 
 }
 
 export default Todo;
-
